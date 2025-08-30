c364053e27a9dc9b1013d44d50b783c72b9ef1e7
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-08-30 22:18:20 +0530

    Create ReverseNumber.c

---
+++ ReverseNumber.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int reverse(int);
+
+int main(){
+    int n;
+    printf("Enter the reverse for reverse ");
+    scanf("%d",&n);
+    printf("the reverse number is %d ",reverse(n));
+    return 0;
+}
+
+int reverse(int n){
+    int rev = 0;
+    while(n>0){
+        int digit = n%10;
+        rev = rev * 10 + digit;
+        n = n/10;
+    }
+    return rev;
+}
