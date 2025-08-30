dc154e5eb8d09e0b4eec874e0fa99c027ceab80c
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-08-30 16:20:06 +0530

    Create ScanAndPrint.c

---
+++ ScanAndPrint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    
+    printf("Enter an integer : ");
+    scanf("%d",&number);
+    
+    printf("you entered : %d \n ",number);
+    return 0;
+}
